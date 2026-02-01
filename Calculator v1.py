0f81c78bd6e16e8802ee55348993175bd34385b6
Harrish Ahilan <148026781+HarrishAhilan@users.noreply.github.com>
2026-01-31 21:49:19 -0800

    Add files via upload
    This modded version allows the user to choose an operator and will also differentiate the variable from floats or integers

--- Calculator v1.py
+++ Calculator v1.py
@@ -1,3 +1,22 @@
-num1 = float(input("What is the first number: "))
-num2 = float(input("What is the second number: "))
-print("the result is",num1+num2)
+def calculator():
+    num1 = float(input("what is the first number: "))
+    num2 = float(input("what is the second number: "))
+    operator = input("What is the operator: ")
+    if operator == "+":
+        result = num1+num2
+    else:
+        if operator in ("x", "*"):
+            result = num1*num2
+        else:
+            if operator == "-":
+                result = num1-num2
+            else:
+                if operator == "/":
+                    result = num1 / num2
+    
+    if result - int(result) == 0:
+        result = int(result)
+    return(result)
+
+
+print(calculator())
